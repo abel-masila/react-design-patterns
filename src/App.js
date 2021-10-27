505790e819f8bc5c90e8e18bfd818da1dbd12050
Abel Masila <abelmasila@gmail.com>
2021-10-28 02:18:33 +0300

    node server added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,5 @@
-import { LargeProductListItem } from './products/LargeProductListItem';
-
-import { Modal } from './Modal';
-
-const products = [
-  {
-    name: 'Flat-Screen TV',
-    price: '$300',
-    description: 'Huge LCD screen, a great deal',
-    rating: 4.5,
-  },
-  {
-    name: 'Basketball',
-    price: '$10',
-    description: 'Just like the pros use',
-    rating: 3.8,
-  },
-  {
-    name: 'Running Shoes',
-    price: '$120',
-    description: 'State-of-the-art technology for optimum running',
-    rating: 4.2,
-  },
-];
-
 function App() {
-  return (
-    <>
-      <Modal>
-        <LargeProductListItem product={products[0]} />
-      </Modal>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
