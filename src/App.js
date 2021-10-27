9e858425a9a2c6f768057e78dc3af1781a885612
Abel Masila <abelmasila@gmail.com>
2021-10-28 01:46:13 +0300

    list components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import { SplitSceen } from './SplitSceen';
+import { SmallPersonListItem } from './people/SmallPersonListItem';
+import { LargePersonListItem } from './people/LargePersonListItem';
+import { RegularList } from './RegularList';
+const people = [
+  {
+    name: 'John Doe',
+    age: 54,
+    hairColor: 'brown',
+    hobbies: ['swimming', 'bicycling', 'video games'],
+  },
+  {
+    name: 'Brenda Smith',
+    age: 33,
+    hairColor: 'black',
+    hobbies: ['golf', 'mathematics'],
+  },
+  {
+    name: 'Jane Garcia',
+    age: 27,
+    hairColor: 'blonde',
+    hobbies: ['biology', 'medicine', 'gymnastics'],
+  },
+];
 
-const LeftHandComponent = ({ name }) => {
-  return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>;
-};
+const products = [
+  {
+    name: 'Flat-Screen TV',
+    price: '$300',
+    description: 'Huge LCD screen, a great deal',
+    rating: 4.5,
+  },
+  {
+    name: 'Basketball',
+    price: '$10',
+    description: 'Just like the pros use',
+    rating: 3.8,
+  },
+  {
+    name: 'Running Shoes',
+    price: '$120',
+    description: 'State-of-the-art technology for optimum running',
+    rating: 4.2,
+  },
+];
 
-const RightHandComponent = ({ message }) => {
-  return <p style={{ backgroundColor: 'red' }}> {message}</p>;
-};
 function App() {
   return (
-    <SplitSceen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent name="Side Nav" />
-      <RightHandComponent message="Main View" />
-    </SplitSceen>
+    <>
+      <RegularList
+        items={people}
+        resourceName="person"
+        itemComponent={SmallPersonListItem}
+      />
+      <RegularList
+        items={people}
+        resourceName="person"
+        itemComponent={LargePersonListItem}
+      />
+    </>
   );
 }
 
