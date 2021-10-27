f60fc47155aa0fb1bcca875ff3d7d375479ab172
Abel Masila <abelmasila@gmail.com>
2021-10-28 02:52:55 +0300

    getLocalStorageData added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import axios from 'axios';
 import { DataSource } from './DataSource';
 import { UserInfo } from './UserInfo';
 import { ProductInfo } from './ProductInfo';
+
+const getServerData = (url) => async () => {
+  const resp = await axios.get(url);
+  return resp.data;
+};
+
+const getLocalStorageData = (key) => () => {
+  return localStorage.getItem(key);
+};
+
+const Text = ({ message }) => <h1>{message}</h1>;
 function App() {
   return (
     <>
-      <DataSource
-        getDataFunc={async () => {
-          const resp = await axios.get('/users/123');
-          return resp.data;
-        }}
-        resourceName="user"
-      >
+      <DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
         <UserInfo />
+      </DataSource>
+      <DataSource
+        getDataFunc={getLocalStorageData('message')}
+        resourceName="message"
+      >
+        <Text />
       </DataSource>
     </>
   );
