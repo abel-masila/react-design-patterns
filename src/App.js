6c93e040b953559419c28688789823cebf03d4d9
Abel Masila <abelmasila@gmail.com>
2021-10-28 02:06:26 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
-import { SmallPersonListItem } from './people/SmallPersonListItem';
-import { LargePersonListItem } from './people/LargePersonListItem';
-import { SmallProductListItem } from './products/SmallProductListItem';
 import { LargeProductListItem } from './products/LargeProductListItem';
-import { RegularList } from './RegularList';
-import { NumberedList } from './NumberedList';
+
 import { Modal } from './Modal';
-const people = [
-  {
-    name: 'John Doe',
-    age: 54,
-    hairColor: 'brown',
-    hobbies: ['swimming', 'bicycling', 'video games'],
-  },
-  {
-    name: 'Brenda Smith',
-    age: 33,
-    hairColor: 'black',
-    hobbies: ['golf', 'mathematics'],
-  },
-  {
-    name: 'Jane Garcia',
-    age: 27,
-    hairColor: 'blonde',
-    hobbies: ['biology', 'medicine', 'gymnastics'],
-  },
-];
 
 const products = [
   {
