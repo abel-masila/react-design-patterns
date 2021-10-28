4a2b4cdd29ccbafc1e5c83d6cc025c1bfaecb88e
Abel Masila <abelmasila@gmail.com>
2021-10-29 00:36:55 +0300

    uncontrolled component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import axios from 'axios';
-import { DataSource } from './DataSource';
-import { UserInfo } from './UserInfo';
-import { ProductInfo } from './ProductInfo';
-
-const getServerData = (url) => async () => {
-  const resp = await axios.get(url);
-  return resp.data;
-};
-
-const getLocalStorageData = (key) => () => {
-  return localStorage.getItem(key);
-};
-
-const Text = ({ message }) => <h1>{message}</h1>;
+import { UncontrolledForm } from './UncontrolledForm';
 function App() {
-  return (
-    <>
-      <DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
-        <UserInfo />
-      </DataSource>
-      <DataSource
-        getDataFunc={getLocalStorageData('message')}
-        resourceName="message"
-      >
-        <Text />
-      </DataSource>
-    </>
-  );
+  return <UncontrolledForm />;
 }
 
 export default App;
---
+++ src/UncontrolledForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+export const UncontrolledForm = () => {
+  const nameInput = React.createRef();
+  const ageInput = React.createRef();
+  const hairColorInput = React.createRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(nameInput.current.value);
+    console.log(ageInput.current.value);
+    console.log(hairColorInput.current.value);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="name" type="text" placeholder="Name" ref={nameInput} />
+      <input name="age" type="number" placeholder="Age" ref={ageInput} />
+      <input
+        name="hairColor"
+        type="text"
+        placeholder="Hair Color"
+        ref={hairColorInput}
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
